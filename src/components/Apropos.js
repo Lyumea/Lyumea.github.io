1aa74e1d43bfe8923f060ae49f0c6033950f2714
Lyumea <tamiraadam@gmail.com>
2023-09-27 13:51:33 +0200

    last last

--- src/components/Apropos.js
+++ src/components/Apropos.js
@@ -9,7 +9,7 @@ const Apropos = () =>{
                     <div className="apropos-text">
                         <h2>NAI</h2>
                         <p>Après avoir fait 2 ans d'expérience dans le métier du design WEB et du design UX/UI, il est grand temps de mettre ces services à l'ouvrage! Grâce à cette expérience et à ma curiosité, j’ai pu apprendre de nombreuses technologies en programmation et en créativité.
-                            Je pourrai alors vous aider dans votre communication visuel, votre stratégie marketing et la promotion de votre produit et de votre service. Vous voulez en savoir plus ? Vous pouvez voir mon Portfolio ou parlons-en m'envoyant <div><Link to="/contact">un petit message !</Link></div></p>
+                            Je pourrai alors vous aider dans votre communication visuel, votre stratégie marketing et la promotion de votre produit et de votre service. Vous voulez en savoir plus ? Vous pouvez voir mon Portfolio ou parlons-en m'envoyant <Link to="/contact">un petit message !</Link></p>
                     </div>
 
                     <div className="apropos-img">
--- src/components/slider/SliderPortfolio.js
+++ src/components/slider/SliderPortfolio.js
@@ -1,6 +1,7 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import useSliderPortfolio from "../../hooks/useSliderPortfolio";
 import { NavLink } from 'react-router-dom';
+// import { useSwipeable } from "react-swipeable";
 
 const SliderPortfolio = ({categories}) =>{
   const slideImage = useRef(null);
@@ -22,9 +23,30 @@ const SliderPortfolio = ({categories}) =>{
     (e.deltaY > 0) ? goToNextSlide() : goToPreviousSlide();
   }
 
+  const [touchStart, setTouchStart] = useState(null)
+  const [touchEnd, setTouchEnd] = useState(null)
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50 
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+  }
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return
+    const distance = touchStart - touchEnd
+    const isLeftSwipe = distance > minSwipeDistance
+    const isRightSwipe = distance < -minSwipeDistance
+    if (isLeftSwipe || isRightSwipe) isLeftSwipe ? goToNextSlide() : goToPreviousSlide()
+    // add your conditional logic here
+  }
 
   return(
-    <div className="slider"  onWheel={handleWheel}>
+    <div className="slider" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd} onWheel={handleWheel}>
       {/*Image de fond*/}
       <div ref={slideImage} className="slider-bg"></div>
       {/*Navigation du slider WEB*/}
